479d6179c541e6c6abdab7026daf9f1a508ef9cf
NStefan002 <n.stefan002@gmail.com>
2023-02-08 04:27:37 +0100

    neovide color scheme change

--- lua/my_config/neovide.lua
+++ lua/my_config/neovide.lua
@@ -5,7 +5,7 @@ if vim.g.neovide then
     vim.g.neovide_fullscreen = true
     vim.g.neovide_cursor_vfx_mode = "pixeldust"
     vim.g.neovide_cursor_vfx_mode = "sonicboom"
-    vim.cmd.colorscheme("minimalist")
+    vim.cmd.colorscheme("hybrid_reverse")
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none"} )
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"} )
 end
