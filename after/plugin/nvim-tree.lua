f1e2b1fd8188caabedfbe5d2b34965df2df3af33
NStefan002 <n.stefan002@gmail.com>
2023-02-13 03:08:58 +0100

    nvim-tree setup almost finished

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,5 +1,3 @@
--- examples for your init.lua
-
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -10,7 +8,7 @@ vim.opt.termguicolors = true
 -- empty setup using defaults
 -- require("nvim-tree").setup()
 
--- OR setup with some options
+-- setup with some options
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
@@ -22,13 +20,73 @@ require("nvim-tree").setup({
         },
     },
     renderer = {
-        group_empty = true,
+        add_trailing = true, -- add '/' at the end of directory name
+        group_empty = true, -- compact folders that only contain a single folder into one node in the file tree
+        full_name = false, -- disable showing very long names in floating window
+        highlight_git = true,
+        highlight_opened_files = "name",
+        symlink_destination = true,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = false,
+            resize_window = true,
+            window_picker = { -- for example when using :vsplit
+                enable = true,
+                picker = "default",
+            },
+        },
+        remove_file = {
+            close_window = true,
+        },
+        use_system_clipboard = true,
+    },
+    ui = {
+        confirm = {
+            remove = true,
+            trash = true,
+        },
     },
     filters = {
         dotfiles = true,
     },
+    auto_reload_on_write = true,
+    hijack_unnamed_buffer_when_opening = false,
+    hijack_cursor = true, -- keeps the cursor on the first letter of the filename
+
+    update_focused_file = {
+        enable = true,
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+        severity = {
+            min = vim.diagnostic.severity.WARNING,
+            max = vim.diagnostic.severity.ERROR,
+        },
+    },
+    system_open = {
+        cmd = ""
+    },
+    git = {
+        enable = true,
+        ignore = true, -- togle mapping is I
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+    },
+    modified = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+    },
+    filesystem_watchers = { -- improves performance
+        enable = true,
+    },
 })
 
+-- mappings (written in remap.lua)
+-- vim.keymap.set("n", "C", "<cmd>NvimTreeCollapse<CR>")
 
 -- require("nvim-web-devicons").set_icon {
 --     zsh = {
--- lua/my_config/remap.lua
+++ lua/my_config/remap.lua
@@ -50,5 +50,9 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
---
+
+-- Packer
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/my_config/packer.lua<CR>");
+
+-- NvimTree
+vim.keymap.set("n", "C", "<cmd>NvimTreeCollapse<CR>")
