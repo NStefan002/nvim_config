906063bb696de31fabcc10cc264e4672ddfbba20
NStefan002 <n.stefan002@gmail.com>
2023-07-31 17:48:56 +0200

    remove deprecated icons

--- lua/plugins/color-picker.lua
+++ lua/plugins/color-picker.lua
@@ -1,8 +1,7 @@
 return {
-    -- color picker (similar to VSC)
+    -- color picker
     {
         "ziontee113/color-picker.nvim",
-        lazy = true,
         keys = {
             { "<C-c>", "<cmd>PickColor<CR>", noremap = true, silent = true, desc = "color-picker: PickColor" },
         },
@@ -13,7 +12,7 @@ return {
                 { noremap = true, silent = true, desc = "color-picker: PickColorInsert" })
 
             require("color-picker").setup({
-                ["icons"] = { "ﱢ", "" },
+                ["icons"] = { "󰝤", "󰃉" },
                 ["border"] = "rounded", -- none | single | double | rounded | solid | shadow
                 ["background_highlight_group"] = "Normal",
                 ["border_highlight_group"] = "FloatBorder",
