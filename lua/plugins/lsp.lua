f9e6bc14cd8c39ca1e366b8ce8bc709dd38f2e4d
NStefan002 <n.stefan002@gmail.com>
2024-02-02 15:03:36 +0100

    just use intellij

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -451,22 +451,4 @@ return {
             })
         end,
     },
-    {
-        "nvim-java/nvim-java",
-        ft = { "java" },
-        dependencies = {
-            "nvim-java/lua-async-await",
-            "nvim-java/nvim-java-core",
-            "nvim-java/nvim-java-test",
-            "nvim-java/nvim-java-dap",
-            "MunifTanjim/nui.nvim",
-            "neovim/nvim-lspconfig",
-            "mfussenegger/nvim-dap",
-            "williamboman/mason.nvim",
-            config = function()
-                require("java").setup()
-                require("lspconfig").jdtls.setup({})
-            end,
-        },
-    },
 }
