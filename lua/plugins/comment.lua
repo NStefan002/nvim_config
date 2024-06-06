342fa5f03429c39c6bf3bb199502ce11b9479058
NStefan002 <n.stefan002@gmail.com>
2024-06-06 15:22:38 +0200

    few cleanups because 0.9.5 is no longer in use

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,6 @@
 return {
     "numToStr/Comment.nvim",
     event = "VeryLazy",
-    -- cond = function()
-    --     return vim.version().minor < 10
-    -- end,
     opts = {
         padding = true,
         sticky = true,
