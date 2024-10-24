783a960f753d7638fec10bad801f6dacf2650b09
NStefan002 <n.stefan002@gmail.com>
2024-10-25 00:52:24 +0200

    function for toggling markdown checkboxes

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,19 @@
 vim.opt.wrap = false
+
+local api = vim.api
+local function toggle_checkbox()
+    local line = api.nvim_get_current_line()
+    if string.match(line, "%[%s%]") then
+        local new_line, _ = line:gsub("%[%s%]", "[x]")
+        api.nvim_set_current_line(new_line)
+    elseif string.match(line, "%[x%]") then
+        local new_line, _ = line:gsub("%[x%]", "[ ]")
+        api.nvim_set_current_line(new_line)
+    end
+end
+vim.keymap.set(
+    "n",
+    "<leader>tc",
+    toggle_checkbox,
+    { buffer = true, desc = "Toggle markdown checkboxes" }
+)
