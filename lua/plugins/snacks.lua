214977134ff48eb3a03d77e7b279e85ec3250897
NStefan002 <n.stefan002@gmail.com>
2024-11-19 00:28:05 +0100

    update snacks.nvim, use `snacks.notifier` instead of `notify.nvim`
    because of that remove telescope completely

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,12 +7,41 @@ return {
         ---@module "snacks"
         ---@type snacks.Config
         local opts = {
-            styles = { lazygit = { border = "double" } },
-            bigfile = { enabled = false },
-            notifier = { enabled = false },
+            styles = {
+                lazygit = { border = "double" },
+
+                ["notification.history"] = {
+                    border = "rounded",
+                    minimal = true,
+                    title = "Notification History",
+                    title_pos = "right",
+                    ft = "markdown",
+                    bo = { filetype = "snacks_notif_history", modifiable = false },
+                    wo = { winhighlight = "Normal:SnacksNotifierHistory", wrap = false },
+                    keys = { q = "close", ["<c-c>"] = "close" },
+                },
+            },
+
             quickfile = { enabled = true },
+
+            lazygit = { enabled = true },
+
+            gitbrowse = { enabled = true },
+
+            notifier = {
+                enabled = true,
+                timeout = 7000,
+                margin = { top = 0, right = 1, bottom = 0 },
+                padding = false,
+                sort = { "level", "added" }, -- sort by level and time
+                ---@type snacks.notifier.style
+                style = "compact",
+                refresh = 50, -- refresh at most every 50ms
+            },
+
             statuscolumn = {
-                left = { "sign", "mark" }, -- priority of signs on the left (high to low)
+                enabled = true,
+                left = { "sign" }, -- priority of signs on the left (high to low)
                 right = { "fold", "git" }, -- priority of signs on the right (high to low)
                 folds = {
                     open = true, -- show open fold icons
@@ -24,24 +53,46 @@ return {
                 },
                 refresh = 50, -- refresh at most every 50ms
             },
-            words = { enabled = false },
         }
 
         local snacks = require("snacks")
         snacks.setup(opts)
 
-        -- vim.keymap.set("n", "<leader>no", function()
-        --     snacks.notifier.hide()
-        -- end, {
-        --     desc = "Dismiss All Notifications",
-        -- })
+        -- notifier
 
+        -- use snacks.notifier as a default way of showing notifications
+        vim.notify = snacks.notifier.notify
+
+        -- redirect print (lua func) to snacks.notifier
+        print = function(...)
+            local print_safe_args = {}
+            local args = { ... }
+            for i = 1, #args do
+                table.insert(print_safe_args, tostring(args[i]))
+            end
+            vim.notify(table.concat(print_safe_args, " "), vim.log.levels.INFO)
+        end
+
+        vim.keymap.set("n", "<leader>nh", function()
+            snacks.notifier.show_history()
+        end, {
+            desc = "Get notification history",
+        })
+
+        vim.keymap.set("n", "<leader>no", function()
+            snacks.notifier.hide()
+        end, {
+            desc = "Dismiss All Notifications",
+        })
+
+        -- lazygit
         vim.keymap.set("n", "<leader>gg", function()
             snacks.lazygit()
         end, {
             desc = "Lazygit",
         })
 
+        -- gitbrowse
         vim.keymap.set("n", "<leader>gB", function()
             snacks.gitbrowse()
         end, {
