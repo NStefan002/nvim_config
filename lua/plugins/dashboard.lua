124d3997b15960b49fd8b71adb807eb0f464e665
NStefan002 <n.stefan002@gmail.com>
2024-12-10 00:10:56 +0100

    remove mini.sessions

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -43,14 +43,6 @@ return {
                         action = "Lazy profile",
                         key = "p",
                     },
-                    {
-                        icon = " ",
-                        icon_hl = "@property",
-                        desc = "Sessions",
-                        group = "@property",
-                        action = "lua MiniSessions.select()",
-                        key = "s",
-                    },
                 },
             },
         })
