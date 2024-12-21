f77bfc8f0d6830e95f2212419d522c4fc24ca35c
NStefan002 <n.stefan002@gmail.com>
2024-12-21 21:59:42 +0100

    spawn `ghcid` in split wezterm pane for haskell files
    NOTE: currently for one-file-things, could be extended into something
    more serious if needed

---
+++ after/ftplugin/haskell.lua
@@ -0,0 +1,24 @@
+-- NOTE: this is only for a one file stuff, like when solving aoc, could be expanded
+
+if not vim.fn.executable("ghcid") then
+    return
+end
+
+local wezterm = require("my_config.util.wezterm")
+local current_file = vim.api.nvim_buf_get_name(0)
+
+if vim.g.ghcid_split_is_open and vim.g.ghcid_current_file == current_file then
+    return
+end
+
+-- we changed files, so we need to run new ghcid
+if vim.g.ghcid_split_is_open then
+    wezterm.kill_pane(vim.g.ghcid_pane_id)
+    vim.g.ghcid_split_is_open = false
+end
+
+local pane_id = wezterm.split_pane("right", true)
+vim.g.ghcid_pane_id = pane_id
+vim.g.ghcid_split_is_open = true
+wezterm.spawn_command(("ghcid --command 'ghci %s' --test ':main'"):format(current_file), pane_id)
+vim.g.ghcid_current_file = current_file
