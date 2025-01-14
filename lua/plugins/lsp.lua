2dc3b0a9b5f058d3f0ff803442df093f229be543
NStefan002 <n.stefan002@gmail.com>
2025-01-14 03:56:49 +0100

    bump blink.cmp to 0.10.0

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -61,9 +61,15 @@ return {
 
             completion = {
                 list = {
-                    selection = function(ctx)
-                        return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-                    end,
+                    selection = {
+                        preselect = function(ctx)
+                            return ctx.mode ~= "cmdline"
+                                and not require("blink.cmp").snippet_active({ direction = 1 })
+                        end,
+                        auto_insert = function(ctx)
+                            return ctx.mode == "cmdline"
+                        end,
+                    },
                 },
 
                 trigger = {
