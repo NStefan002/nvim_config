760643485fb415bc0f019ff437ce8abb38d34081
NStefan002 <n.stefan002@gmail.com>
2025-01-21 19:47:23 +0100

    use visual-surround to make text italic/bold/both

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 vim.opt.wrap = false
 
 local api = vim.api
+
 local function toggle_checkbox()
     local line = api.nvim_get_current_line()
     if string.match(line, "%[%s%]") then
@@ -17,3 +18,11 @@ vim.keymap.set(
     toggle_checkbox,
     { buffer = true, desc = "Toggle markdown checkboxes" }
 )
+
+local surroundings = { i = "*", b = "**", a = "***" } -- italic, bold, bold&italic
+local prefix = "s"
+for k, v in pairs(surroundings) do
+    vim.keymap.set("v", prefix .. k, function()
+        require("visual-surround").surround(v, v)
+    end, { buffer = true, desc = "[visual-surround] Surround selection with " .. v })
+end
