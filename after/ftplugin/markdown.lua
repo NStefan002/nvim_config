cb09ae34dcb627e4cbba77b2839552ef3b78129a
NStefan002 <n.stefan002@gmail.com>
2025-02-01 01:02:17 +0100

    generate table of contents in markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -26,3 +26,25 @@ for k, v in pairs(surroundings) do
         require("visual-surround").surround(v, v)
     end, { buffer = true, desc = "[visual-surround] Surround selection with " .. v })
 end
+
+local function generate_toc()
+    local toc = {}
+    local lines = api.nvim_buf_get_lines(0, 0, -1, false)
+    for _, line in ipairs(lines) do
+        local level = string.match(line, "^#+")
+        if level then
+            local title = string.match(line, "^#+%s+(.+)")
+            local link = string.gsub(title, "%s+", "-"):lower()
+            table.insert(
+                toc,
+                string.rep("  ", #level - 1) .. "- [" .. title .. "](#" .. link .. ")"
+            )
+        end
+    end
+    local current_line = api.nvim_win_get_cursor(0)[1] - 1
+    api.nvim_buf_set_lines(0, current_line, current_line, false, toc)
+end
+
+vim.keymap.set("n", "<leader>toc", function()
+    generate_toc()
+end, { buffer = true, desc = "Generate table of contents" })
