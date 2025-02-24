47aa4f4a2d62f2e576c66ed5bdf83c1ca83fc152
NStefan002 <n.stefan002@gmail.com>
2025-02-24 14:25:03 +0100

    use codecompanion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,49 +1,60 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    keys = {
-        {
-            "<leader>ec",
-            "<cmd>Copilot enable<CR>",
-            desc = "[E]nable [C]opilot",
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        keys = {
+            {
+                "<leader>ec",
+                "<cmd>Copilot enable<CR>",
+                desc = "[E]nable [C]opilot",
+            },
+            {
+                "<leader>dc",
+                "<cmd>Copilot disable<CR>",
+                desc = "[D]isable [C]opilot",
+            },
         },
-        {
-            "<leader>dc",
-            "<cmd>Copilot disable<CR>",
-            desc = "[D]isable [C]opilot",
+        config = function()
+            require("copilot").setup({
+                panel = {
+                    enabled = false,
+                },
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<C-a>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<C-'>",
+                        prev = '<C-">',
+                        dismiss = "<C-x>",
+                    },
+                },
+                filetypes = {
+                    yaml = false,
+                    markdown = true,
+                    help = false,
+                    gitcommit = true,
+                    gitrebase = false,
+                    hgcommit = false,
+                    svn = false,
+                    cvs = false,
+                    ["."] = false,
+                },
+                copilot_node_command = "node", -- Node.js version must be > 18.x
+                server_opts_overrides = {},
+            })
+        end,
+    },
+    {
+        "olimorris/codecompanion.nvim",
+        cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionCmd", "CodeCompanionActions" },
+        config = true,
+        dependencies = {
+            { "nvim-lua/plenary.nvim", brach = "master" },
+            "nvim-treesitter/nvim-treesitter",
         },
     },
-    config = function()
-        require("copilot").setup({
-            panel = {
-                enabled = false,
-            },
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                debounce = 75,
-                keymap = {
-                    accept = "<C-a>",
-                    accept_word = false,
-                    accept_line = false,
-                    next = "<C-'>",
-                    prev = '<C-">',
-                    dismiss = "<C-x>",
-                },
-            },
-            filetypes = {
-                yaml = false,
-                markdown = true,
-                help = false,
-                gitcommit = false,
-                gitrebase = false,
-                hgcommit = false,
-                svn = false,
-                cvs = false,
-                ["."] = false,
-            },
-            copilot_node_command = "node", -- Node.js version must be > 18.x
-            server_opts_overrides = {},
-        })
-    end,
 }
