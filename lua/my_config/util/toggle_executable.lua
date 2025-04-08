6fd43cf289e2614e3fdd38dbd3a8fbe178080223
NStefan002 <n.stefan002@gmail.com>
2025-04-08 03:24:14 +0200

    use `vim.notify` when fidget is not available

--- lua/my_config/util/toggle_executable.lua
+++ lua/my_config/util/toggle_executable.lua
@@ -4,13 +4,16 @@ function M.toggle()
     local file = vim.fn.expand("%:p")
     local short_file = vim.fn.expand("%:t")
     local is_executable = vim.fn.getfperm(file):find("x") ~= nil
-    local notify = require("fidget").notify
+    -- check if the fidget plugin is loaded and if not use the default vim.notify
+    local loaded, _ = pcall(require, "fidget")
+    local notify = loaded and require("fidget").notify or vim.notify
+    local opts = loaded and { skip_history = true } or {}
     if is_executable then
         vim.system({ "chmod", "-x", file }, {}):wait()
-        notify(("chmod -x %s"):format(short_file), vim.log.levels.INFO, { skip_history = true })
+        notify(("chmod -x %s"):format(short_file), vim.log.levels.INFO, opts)
     else
         vim.system({ "chmod", "+x", file }, {}):wait()
-        notify(("chmod +x %s"):format(short_file), vim.log.levels.INFO, { skip_history = true })
+        notify(("chmod +x %s"):format(short_file), vim.log.levels.INFO, opts)
     end
 end
 
