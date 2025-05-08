2fadf14e12418a735fd377881be6f56489fc4f42
NStefan002 <n.stefan002@gmail.com>
2025-05-08 03:40:31 +0200

    use `nvim_echo` to notify

--- lua/my_config/util/toggle_executable.lua
+++ lua/my_config/util/toggle_executable.lua
@@ -5,15 +5,20 @@ function M.toggle()
     local short_file = vim.fn.expand("%:t")
     local is_executable = vim.fn.getfperm(file):find("x") ~= nil
     -- check if the fidget plugin is loaded and if not use the default vim.notify
-    local loaded, _ = pcall(require, "fidget")
-    local notify = loaded and require("fidget").notify or vim.notify
-    local opts = loaded and { skip_history = true } or {}
     if is_executable then
         vim.system({ "chmod", "-x", file }, {}):wait()
-        notify(("chmod -x %s"):format(short_file), vim.log.levels.INFO, opts)
+        vim.api.nvim_echo({
+            { "chmod", "" },
+            { " -x ", "DiagnosticVirtualTextError" },
+            { short_file, "Directory" },
+        }, false, { verbose = false })
     else
         vim.system({ "chmod", "+x", file }, {}):wait()
-        notify(("chmod +x %s"):format(short_file), vim.log.levels.INFO, opts)
+        vim.api.nvim_echo({
+            { "chmod", "" },
+            { " +x ", "DiagnosticVirtualTextOk" },
+            { short_file, "Directory" },
+        }, false, { verbose = false })
     end
 end
 
