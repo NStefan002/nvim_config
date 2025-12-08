ef262010f2ae6e81a8ebd4d071c8ce52eb6250f4
NStefan002 <n.stefan002@gmail.com>
2025-12-08 23:46:10 +0100

    use R.nvim for R language support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -406,4 +406,44 @@ return {
             vim.filetype.add({ extension = { ejs = "ejs" } })
         end,
     },
+    {
+        "R-nvim/R.nvim",
+        lazy = false,
+        config = function()
+            ---@type RConfigUserOpts
+            local opts = {
+                hook = {
+                    on_filetype = function()
+                        vim.keymap.set("n", "<enter>", "<Plug>RDSendLine", { buffer = true })
+                        vim.keymap.set("v", "<enter>", "<Plug>RSendSelection", { buffer = true })
+                    end,
+                },
+                R_args = { "--quiet", "--no-save" },
+                min_editor_width = 72,
+                rconsole_width = 78,
+                objbr_mappings = { -- Object browser keymap
+                    c = "class", -- Call R functions
+                    ["<localleader>gg"] = "head({object}, n = 15)", -- Use {object} notation to write arbitrary R code.
+                    v = function()
+                        -- Run lua functions
+                        require("r.browser").toggle_view()
+                    end,
+                },
+                disable_cmds = {
+                    -- "RClearConsole",
+                    "RCustomStart",
+                    -- "RSPlot",
+                    "RSaveClose",
+                },
+            }
+            -- Check if the environment variable "R_AUTO_START" exists.
+            -- If using fish shell, you could put in your config.fish:
+            -- alias r "R_AUTO_START=true nvim"
+            if vim.env.R_AUTO_START == "true" then
+                opts.auto_start = "on startup"
+                opts.objbr_auto_start = true
+            end
+            require("r").setup(opts)
+        end,
+    },
 }
